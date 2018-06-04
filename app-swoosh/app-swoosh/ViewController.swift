6082303f1ced38e6a3dcd5c6b7294b2f55363a37
omDroid <omdroid@omDroids-Mac-mini.local>
2018-06-05 00:31:21 +0430

    sqgues unwind

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -20,9 +20,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSqgue : UIStoryboardSegue){
+        
     }
 
 
